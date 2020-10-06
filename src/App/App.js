20de158c7d55743da0276f99b80ed85aa3d9f25b
Monique.Bass01 <monique.bass01@gmail.com>
2020-10-06 13:46:34 -0500

    cards are working

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { Provider } from 'react-redux';
+// import productData from '../helpers/data/productData';
+import Products from '../components/Products';
+// import Filter from '../components/Filter';
+// import Basket from '../components/Basket';
+// import store from '../store';
 import './App.scss';
+// import Copyright from '../components/Copyright';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [], filteredProducts: [] };
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:3000/products').then((res) => res.json())
+      .then((data) => this.setState({
+        products: data,
+        filteredProducts: data,
+      }));
+  }
+
+  // componentDidMount() {
+  //   const products = productData.getProducts();
+  //   this.setState({ products });
+  //   console.log('products', products);
+  // }
+
   render() {
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
-      </div>
+      <div className="container">
+          <h1>E-commerce Shopping Cart Application</h1>
+           <div className="row">
+            <div className="col-md-8">
+             <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
+            </div>
+            </div>
+            </div>
     );
   }
 }
